25ea436d091bac9f4afc5b01c21f94f12cbe9d04
guillaume <ductchmun@gmail.com>
2021-10-08 22:59:11 +0100

    initial file commit

---
+++ BankAccount.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMConsole
+{
+    public class BankAccount
+    {
+
+        private int Account_NR;
+        private string First_Name;
+        private string Last_Name;
+        private int Sort_Code;
+        private string PIN_Number;
+        private double Balance;
+
+
+        //constructor with test data
+        public BankAccount()
+        {
+            Account_NR = 1234567890;
+            First_Name = "Mark";
+            Last_Name = "Markson";
+            Sort_Code = 556688;
+            PIN_Number = PIN;
+        }
+
+        public string PIN
+        {
+            get { return PIN_Number; }
+            set
+            {
+                if (value.Length == 4 && int.TryParse(value, out _) == true)
+                {
+                    PIN_Number = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid nr");
+                    throw new InvalidPINException(" Number is invalid.");
+                }
+            }
+        }
+
+
+
+        public bool CheckPIN()
+        {
+            bool statusCheck = false;
+            Console.WriteLine("Please enter PIN number");
+            try
+            {
+                int INPUT_To_Check = Convert.ToInt32(Console.ReadLine());
+
+                if (INPUT_To_Check == Convert.ToInt32(PIN_Number))
+                {
+                    Console.WriteLine("Correct PIN: ");
+                    statusCheck = true;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect PIN: ");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Not a PIN number");
+            }
+            return statusCheck;
+        }
+
+        public void ChangePIN()
+        {
+            //verifies PIN before changing PIN value
+            Console.WriteLine("Changing PIN\n");
+            if (this.CheckPIN() == true)
+            {
+                //checks input for new PIN twice to ensure accuracy
+                while (true)
+                {
+                    Console.WriteLine("Please enter a new PIN");
+                    string newPIN = Console.ReadLine();
+                    Console.WriteLine("Confirm PIN");
+                    if (newPIN == Console.ReadLine())
+                    {
+                        this.PIN = newPIN;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("PIN does not match");
+                    }
+                }
+            }        
+        }
+
+        public void Withdraw()
+        { }
+
+        public void Deposit()
+        { }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+
+/*
+ *  Checking whether an input – such as an ATM card (a debit/credit card number) – is recorded correctly
+    Verifying the user by asking for a PIN
+    In case of negative verification, logging out the user
+    In case of positive verification, showing multiple options, including cash availability, the previous five transactions, and cash withdrawal
+    Giving the user the ability to withdraw up to $1,000 worth of cash in one transaction, with total transactions limited to ten per day.
+
+For a more complicated program, include the ability to register a new PIN and mobile number,
+a detailed bank statement, and a “fast” cash withdrawal system for quickly withdrawing $20, $50, or $100.
+ * 
+ * 
+ */
+namespace ATMConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello World!");
+
+                BankAccount Account1 = new BankAccount();
+                try
+                {
+                    Account1.PIN = "2245";
+                }
+                catch (Exception e)
+                {
+                    // Something unexpected went wrong.
+                    Console.WriteLine(e);
+                    // Maybe it is also necessary to terminate / restart the application.
+                }
+
+
+                Console.WriteLine(Account1.PIN);
+                Account1.ChangePIN();
+                Console.WriteLine("test");
+                Console.WriteLine(Account1.PIN);
+
+                break;
+
+                /*
+                if (Account1.CheckPIN() == true)
+                { 
+                    
+                }
+                */
+                /*
+                if (CheckPIN() == true)
+                {
+                    BankingMenu();
+                }*/
+
+            }
+        }      
+    }
+
+    public class InvalidPINException : Exception
+    {
+        public InvalidPINException()
+        { 
+        }
+
+        public InvalidPINException(string message)
+            :base(message)
+        {
+        }
+
+        public InvalidPINException(string message, Exception inner)
+            :base(message, inner)
+        {
+        }
+    }
+}
